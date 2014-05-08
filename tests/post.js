3051ae059a9300d9aac64ee994e7adf476b39e62
Antonina Kasim <akasim@vast.com>
2014-05-08 16:19:26 +0300

    changed path for pipeline dependency to vast repository and added test from here: https://github.com/tommymessbauer/pipeline-router/pull/11/files

--- tests/post.js
+++ tests/post.js
@@ -2,60 +2,61 @@ var Router = require('../index.js');
 var MockRequest = require('hammock').Request;
 var MockResponse = require('hammock').Response;
 var assert = require('assert');
+var _ = require('lodash');
 
 var routerFactory = {
-	create: function() {
-		var router = new Router();
+  create: function() {
+    var router = new Router();
 
-		router.param('name', /(\w+)/);
+    router.param('name', /(\w+)/);
 
-		router.post('/kitten/:name', function(httpContext) {
-			var req = httpContext.request, 
-					res = httpContext.response;
-					
-			res.writeHead(200, { 'Content-Type': 'application/json' });
+    router.post('/kitten/:name', function(httpContext) {
+      var req = httpContext.request,
+          res = httpContext.response;
+
+      res.writeHead(200, { 'Content-Type': 'application/json' });
       res.write(
-      	JSON.stringify({
-					params: httpContext.params,
-					query: httpContext.query,
-					body: httpContext.body
-				})
-			);
+          JSON.stringify({
+            params: httpContext.params,
+            query: httpContext.query,
+            body: httpContext.body
+          })
+      );
       res.end();
-		});
+    });
 
-		return router;
-	}
+    return router;
+  }
 };
 
 suite('router POST', function() {
 
   test("test post with multiple params", function(done) {
-  	var router = routerFactory.create();
+    var router = routerFactory.create();
     var body = new Buffer('one=foo&two=goo', 'utf-8');
-  	var req = new MockRequest({
-          url: '/kitten/bubbles',
-          headers: { host: 'localhost', 'content-type': 'application/x-www-form-urlencoded', 'content-length': body.length },
-          method: 'POST'
-      });
-    var res = new MockResponse();	
+    var req = new MockRequest({
+      url: '/kitten/bubbles',
+      headers: { host: 'localhost', 'content-type': 'application/x-www-form-urlencoded', 'content-length': body.length },
+      method: 'POST'
+    });
+    var res = new MockResponse();
 
-  	router.dispatch(req, res);
+    router.dispatch(req, res);
 
-  	// this will write data to request body
-  	req.end(body);
+    // this will write data to request body
+    req.end(body);
 
     res.on('end', function(err, data) {
-    	var json = JSON.parse(data.body);
-    	// console.log(data);
+      var json = JSON.parse(data.body);
+      // console.log(data);
 
-    	assert.ok(json.body.one, "ensure 'one' exists");
-    	assert.equal(json.body.one, 'foo', "Check value of 'one'");
+      assert.ok(json.body.one, "ensure 'one' exists");
+      assert.equal(json.body.one, 'foo', "Check value of 'one'");
 
-    	assert.ok(json.body.two, "ensure 'two' exists");
-  		assert.equal(json.body.two, 'goo', "Check value of 'two'");
+      assert.ok(json.body.two, "ensure 'two' exists");
+      assert.equal(json.body.two, 'goo', "Check value of 'two'");
 
-    	done();
+      done();
     });
   });
 
@@ -63,11 +64,11 @@ suite('router POST', function() {
     var router = routerFactory.create();
     var body = new Buffer('{ "one": "foo", "two": "goo" }', 'utf-8');
     var req = new MockRequest({
-          url: '/kitten/bubbles',
-          headers: { host: 'localhost', 'content-type': 'application/json', 'content-length': body.length },
-          method: 'POST'
-      });
-    var res = new MockResponse(); 
+      url: '/kitten/bubbles',
+      headers: { host: 'localhost', 'content-type': 'application/json', 'content-length': body.length },
+      method: 'POST'
+    });
+    var res = new MockResponse();
 
     router.dispatch(req, res);
 
@@ -86,5 +87,77 @@ suite('router POST', function() {
 
       done();
     });
+  });
+
+  test("test POST from route table with GET coming first.", function(done) {
+    var router = new Router();
+    var handler = function(httpContext) {
+      var req = httpContext.request,
+          res = httpContext.response;
+
+      res.writeHead(200, { 'Content-Type': 'application/json' });
+      res.write(
+          JSON.stringify({
+            params: httpContext.params,
+            query: httpContext.query,
+            body: httpContext.body
+          })
+      );
+      res.end();
+    };
+
+    var routes = {
+      "someGetRoute": {
+        "method": "GET",
+        "name": "someGetRoute",
+        "path": "/someGetRoute",
+        "description": "Example GET route",
+        "handler": handler,
+        "params": {},
+        "enabled": true
+      },
+      "somePostRoute": {
+        "method": "POST",
+        "name": "somePostRoute",
+        "path": "/somePostRoute",
+        "description": "Example POST route",
+        "handler": handler,
+        "params": {},
+        "enabled": true
+      }
+    };
+
+
+    _.each(routes, function (route, key) {
+      router.use(route.method,route.path,{ timeout: route.timeout },handler);
+    });
+
+
+    var body = new Buffer('{ "one": "foo", "two": "goo" }', 'utf-8');
+    var req = new MockRequest({
+      url: '/somePostRoute',
+      headers: { host: 'localhost', 'content-type': 'application/json', 'content-length': body.length },
+      method: 'POST'
+    });
+    var res = new MockResponse();
+
+    router.dispatch(req, res);
+
+    // this will write data to request body
+    req.end(body);
+
+    res.on('end', function(err, data) {
+      var json = JSON.parse(data.body);
+
+      assert.ok(json.body.one, "ensure 'one' exists");
+      assert.equal(json.body.one, 'foo', "Check value of 'one'");
+
+      assert.ok(json.body.two, "ensure 'two' exists");
+      assert.equal(json.body.two, 'goo', "Check value of 'two'");
+
+      done();
+    });
+
+
   });
 });
